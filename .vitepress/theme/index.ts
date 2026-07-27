20295077a1bf98589ece7eb55bc7b157ffd053ab
gfccc <cc3428742026@gmail.com>
2026-07-27 22:17:10 +0800

    二维叉积

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,9 +3,7 @@ import DefaultTheme from 'vitepress/theme'
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
 import CustomLayout from './components/CustomLayout.vue'
 import StepViewer from './components/StepViewer.vue'
-import HomePage from './components/HomePage.vue'
 import HomeContent from './components/HomeContent.vue'
-import AlgoNav from './components/AlgoNav.vue'
 import './custom.css'
 
 export default {
@@ -14,8 +12,6 @@ export default {
   enhanceApp({ app }) {
     enhanceAppWithTabs(app)
     app.component('StepViewer', StepViewer)
-    app.component('HomePage', HomePage)
     app.component('HomeContent', HomeContent)
-    app.component('AlgoNav', AlgoNav)
   },
 } satisfies Theme
